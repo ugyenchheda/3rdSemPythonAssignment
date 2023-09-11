6c2b1f80cbedff3926b0d2934461783c7f0da52d
ugyenchhedaugyenchhe <ugyenchheda@gmail.com>
2023-09-11 23:38:02 +0300

    changes in blend to read data

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 import os
-
 altitude_file_path = os.path.join(os.getcwd(), 'fileOne.asc')
 
 try:
     with open(altitude_file_path, 'r') as altitude:
-        # Read the first thirty rows
-        for _ in range(3):
+        for _ in range(30):
             line = altitude.readline().strip()
             if not line:
                 break  # End of file reached
             columns = line.split()
 
-            # Read the first thirty columns in each row
             for i, column in enumerate(columns):
-                if i < 3:
+                if i < 30:
                     print(column, end='\t')
-            print()
 
 except FileNotFoundError:
     print(f"The file '{altitude_file_path}' does not exist.")
