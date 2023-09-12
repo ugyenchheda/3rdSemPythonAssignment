8d94ad44cc31ccd3c071efc3fada4b2af584180c
Ugyen C. Lama <86464524+ugyenchheda@users.noreply.github.com>
2023-09-12 23:53:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import os
 altitude_file_path = os.path.join(os.getcwd(), 'fileOne.asc')
-
 try:
     with open(altitude_file_path, 'r') as altitude:
         for _ in range(30):
             line = altitude.readline().strip()
             if not line:
-                break  # End of file reached
+                break  
             columns = line.split()
-
             for i, column in enumerate(columns):
                 if i < 30:
-                    print(column, end='\t')
-
+                    print(column)
 except FileNotFoundError:
     print(f"The file '{altitude_file_path}' does not exist.")
 
